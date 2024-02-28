7db8dfbe4177905181bb4a90f8146c194116d71a
pratibhadixit549 <pratibhadixit549@gmail.com>
2024-02-28 21:30:32 +0530

    Question 1 Solution for DSA Graded Project updated

---
+++ Problem2/src/Problem2/InputTree.java
@@ -0,0 +1,26 @@
+package Problem2;
+
+class InputTree {
+
+	// Initialising variable
+	static TreeNode node;
+
+	static void userInput() {
+		InputTree tree = new InputTree();
+		tree.node.left = new TreeNode(5);
+		tree.node.left.left = new TreeNode(1);
+		tree.node.left.right = new TreeNode(6);
+		tree.node.right = new TreeNode(12);
+		tree.node.right.left = new TreeNode(11);
+		tree.node.right.right = new TreeNode(15);
+
+		// Converting BST to Right Skewed Tree
+		new BSTToSkewed();
+		BSTToSkewed.bstToRightSkewedTree(node);
+
+		// Printing Right Skewed Tree node values present in ascending order
+		BSTToSkewed.printSkewedTree();
+
+	}
+
+}
--- Problem2/src/Problem2/Main.java
+++ Problem2/src/Problem2/Main.java
@@ -2,27 +2,11 @@ package Problem2;
 
 public class Main {
 
-	// Initialising variable
-	static TreeNode node;
-
 	// Entry point
 	public static void main(String[] args) {
 
-		new Main();
-		Main.node = new TreeNode(10);
-		Main.node.left = new TreeNode(5);
-		Main.node.left.left = new TreeNode(1);
-		Main.node.left.right = new TreeNode(6);
-		Main.node.right = new TreeNode(12);
-		Main.node.right.left = new TreeNode(11);
-		Main.node.right.right = new TreeNode(15);
-
-		// Converting BST to Right Skewed Tree
-		new BSTToSkewed();
-		BSTToSkewed.bstToRightSkewedTree(node);
-
-		// Printing Right Skewed Tree node values present in ascending order
-		BSTToSkewed.printSkewedTree();
+		new InputTree();
+		InputTree.userInput();
 
 	}
 
